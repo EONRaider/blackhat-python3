fb97158edbe2fcdca7ba116f74787fd94c2d275e
EONRaider <livewire_voodoo@protonmail.com>
2020-05-11 17:50:49 -0300

    Convert code to Python 3 and apply PEP 8 rules

--- chapter11/code_coverage.py
+++ chapter11/code_coverage.py
@@ -1,21 +1,19 @@
 from immlib import *
 
+
 class cc_hook(LogBpHook):
-    
+
     def __init__(self):
-        
         LogBpHook.__init__(self)
         self.imm = Debugger()
-        
-    def run(self,regs):
 
-        self.imm.log("%08x" % regs['EIP'],regs['EIP'])
-        self.imm.deleteBreakpoint(regs['EIP'])    
-        
-        return 
-    
+    def run(self, regs):
+        self.imm.log("%08x" % regs['EIP'], regs['EIP'])
+        self.imm.deleteBreakpoint(regs['EIP'])
+        return
+
+
 def main(args):
-    
     imm = Debugger()
 
     calc = imm.getModule("calc.exe")
@@ -24,8 +22,8 @@ def main(args):
     functions = imm.getAllFunctions(calc.getCodebase())
 
     hooker = cc_hook()
-    
+
     for function in functions:
         hooker.add("%08x" % function, function)
-            
+
     return "Tracking %d functions." % len(functions)
